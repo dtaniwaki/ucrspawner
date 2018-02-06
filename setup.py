b268b9abe20fdbbcb690427ee1f9457c93e32e9c
Daisuke Taniwaki <daisuketaniwaki@gmail.com>
2018-02-06 19:09:59 +0900

    Set up tox

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import os
 from setuptools import setup
 
 requires = [
-  'jupyterhub>=0.7',
-  'marathon',
+    'jupyterhub>=0.7',
+    'marathon',
 ]
 
 tests_require = [
-  'pytest>=3.0,<3.3',
-  'pytest-cov>=2.5,<3dev',
-  'pytest-html>=1.15.2,<2dev',
+    'pytest>=3.0,<3.3',
+    'pytest-cov>=2.5,<3dev',
+    'pytest-html>=1.15.2,<2dev',
 ]
 
 version_ns = {}
@@ -32,8 +32,23 @@ setup(
     install_requires=requires,
     setup_requires=[
         'pytest-runner>=2.0,<3dev',
+        'tox>=2.7,<3dev',
     ],
     tests_require=tests_require,
+    extras_require={
+        'flake8': [
+            'flake8>=3.5.0,<4dev',
+            'autoflake>=1.1,<2dev',
+            'autopep8>=1.3,<2dev',
+            'flake8-blind-except>=0.1.1,<1dev',
+            'flake8-docstrings>=1.3.0,<2dev',
+            'flake8-import-order>=0.16,<1dev',
+            'flake8-html>=0.4.0,<1dev',
+        ],
+        'cov': [
+            'coverage>=4.5.0,<5dev',
+        ],
+    },
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
